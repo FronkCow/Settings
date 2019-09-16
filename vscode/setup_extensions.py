d6f708874089e5231dfa34c8d0ab3e66b669fee4
Frank Cao <frankyjcao@gmail.com>
2019-09-16 10:26:21 -0400

    reworked setup_extensions.py, using argparse and python file writing

--- vscode/setup_extensions.py
+++ vscode/setup_extensions.py
@@ -1,15 +1,19 @@
-import sys
+#import sys
+import argparse
 import subprocess
 
-arg = sys.argv[1]
-if arg == "-l":
-    cmd = ["code", "--list-extensions", ">|", "extensions.txt"]
-    subprocess.call(cmd)
-elif arg == "-i":
+parser = argparse.ArgumentParser(description="Import vscode extensions.")
+parser.add_argument("-m", help="select mode, \"install\" or \"load\" (to file)")
+args = parser.parse_args()
+if args.m == "load":
+    f = open("extensions.txt", "w")
+    cmd = ["code", "--list-extensions"]
+    subprocess.call(cmd, stdout=f)
+elif args.m == "install":
     f = open("extensions.txt", "r")
     lines = f.readlines()
     for line in lines:
         cmd = ["code", "--install-extension", line.strip("\n")]
         subprocess.call(cmd)
 else:
-    print("unsupported option, please use -l or -i")
+    print("unsupported option, please use \"install\" or \"load\"")
